d938ad9528bece70eae51b73c6ae4c263d43727d
shin-ruby <shin.php@gmail.com>
2015-11-20 22:47:33 +0800

    finish contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'about' => 'static_pages#about'
-
   get 'course' => 'static_pages#course'
+  get 'contact' => 'static_pages#contact'
 
 
 
